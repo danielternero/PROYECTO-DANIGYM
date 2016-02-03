1102e0e139c706ef4ce1e734aed6c881dd4ebb93
daniel <daniel_martin91@hotmail.com>
2016-02-03 12:13:12 +0100

    3/2/2016

---
+++ gym/Proyecto1.php
@@ -0,0 +1,44 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+    <link rel="stylesheet" type="text/css" href="proyecto1.css"/>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css"/>
+    <link href='https://fonts.googleapis.com/css?family=Raleway:500,600' rel='stylesheet' type='text/css'>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script>
+    $(document).ready(function(){
+    $(".lista1").click(function(){
+        alert("The paragraph was clicked.");
+    });
+});
+
+</script>
+  </head>
+<body>
+<div id="contenedor">
+    <div id="cabecera">
+        <div class="cuadro1">
+        <img class="logo" src="Captura.png"/>
+            <ul>
+            <li class="lista1">PLAN</li>
+            <li class="lista1">INSTALACIONES</li>
+            </ul>
+
+
+        </div>
+        <div class="cuadro2">
+        <img class="botoninicio" src="boton-inicio-sesion.png"/>
+        </div>
+    </div>
+    <div id="carrusel"></div>
+</div>
+</body>
+</html>
---
+++ gym/usuario.php
@@ -0,0 +1,46 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+    <link rel="stylesheet" type="text/css" href="usuario.css"/>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css"/>
+    <link href='https://fonts.googleapis.com/css?family=Raleway:500,600' rel='stylesheet' type='text/css'>
+
+
+
+  </head>
+<body>
+<div id="contenedor">
+  <div id="cabecera">
+    <div class="cuadro1">
+      <img  class="logo" src="captura.png"/>
+      <h1 class="welcome">NOMBRE Y APELLIDOS</h1>
+    </div>
+    <div class="cuadro2">
+      <img  class="botoncerrar" src="boton-cerrar-sesion.png"/>
+    </div>
+  </div>
+  <div id="cuerpo">
+  <ul>
+  <li class="lista1">Plan actual: Aumento de Masa Muscular</li>
+  <li class="lista1">Peso: 80kg</li>
+  <li class="lista1">Fecha_Inicio: 03/02/2016</li>
+  <li class="lista1">Fecha_Fin: 03/03/2016</li>
+</ul>
+  </div>
+  <div id="pie">
+  <div>
+</div>
+
+
+
+
+</body>
+</html>

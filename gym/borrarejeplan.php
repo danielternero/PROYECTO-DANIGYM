7ff5c6a2e702806e8686955221ac17a3da5d6833
dani <daniel_martin91@hotmail.com>
2016-03-05 14:25:10 +0100

    ok

--- gym/borrarejeplan.php
+++ gym/borrarejeplan.php
@@ -5,7 +5,7 @@ session_start();
 if (isset($_GET['idplan'])){
 	
  $connection = new mysqli($db_host, $db_user, $db_password, $db_name);
-	$result = $connection->query("DELETE FROM CONFORMA WHERE FKID_PLAN=".$_GET['idplan']." and FKID_EJERCICIO=".$_GET['idejer'].";");
+	$result = $connection->query("DELETE FROM conforma WHERE FKID_PLAN=".$_GET['idplan']." and FKID_EJERCICIO=".$_GET['idejer'].";");
 
 }
 header("Location: adminplan.php");
